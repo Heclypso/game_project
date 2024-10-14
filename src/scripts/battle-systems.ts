f655d090370157b8dc1b5e58cd8682257391bcda
Gabriel Munhak <heclypso@gmail.com>
2024-10-14 14:53:56 -0300

    Desenvolvido sistema de batalha

--- src/scripts/battle-systems.ts
+++ src/scripts/battle-systems.ts
@@ -1,13 +1,18 @@
 // sistema de combate
 
 const enemy = document.querySelectorAll<HTMLElement>('.enemy');
-const emptyHpBar = document.querySelectorAll<HTMLElement>('.card__red__hp');
-const hpBar = document.querySelectorAll<HTMLElement>('.card__green__hp');
-const EnemyHp = document.querySelectorAll<HTMLElement>('.card__hp__points');
+const enemyEmptyHpBar = document.querySelectorAll<HTMLElement>('.enemy__red__hp');
+const enemyHpBar = document.querySelectorAll<HTMLElement>('#enemy-hp');
+const enemyHp = document.querySelectorAll<HTMLElement>('#enemy-hp-points');
 
-const characterHP = document.querySelectorAll<HTMLElement>('.character__hp__points');
-const healthBar = document.querySelectorAll<HTMLElement>('.character__green__hp')
-const characterMP = document.querySelectorAll<HTMLElement>('.character__mana__points');
+const ally = document.querySelectorAll<HTMLElement>('.ally');
+const allyEmptyHpBar = document.querySelectorAll<HTMLElement>('.ally__red__hp');
+const allyHpBar = document.querySelectorAll<HTMLElement>('#ally-hp');
+const allyHp = document.querySelectorAll<HTMLElement>('#ally-hp-points');
+
+const playerHp = document.querySelectorAll<HTMLElement>('.character__hp__points');
+const playerHealthBar = document.querySelectorAll<HTMLElement>('.character__green__hp')
+const playerMp = document.querySelectorAll<HTMLElement>('.character__mana__points');
 const manaBar = document.querySelectorAll<HTMLElement>('.character__mana__value');
 
 const characterUI = document.getElementById('main-character-bars-container') as HTMLElement;
@@ -15,30 +20,19 @@ const characterIMG = document.getElementById('character-image') as HTMLElement;
 const characterNAME = document.getElementById('character-name') as HTMLElement;
 const characterGif = document.getElementById('main-character-gif') as HTMLElement;
 
-const tutorialUI = document.getElementById('main-tutorial-bars-container') as HTMLElement;
-const tutorialIMG = document.getElementById('tutorial-image') as HTMLElement;
-const tutorialNAME = document.getElementById('tutorial-name') as HTMLElement;
-
-
-tutorialUI.style.display = "none";
-
-enemy.forEach((e) =>{
-    e.addEventListener('click', ()=>{
-        EnemyHp.forEach((e) => {
-            e.textContent = "0"
-        })
-        hpBar.forEach((e) => {
-            e.style.width = '0%'; 
-        })
-    })
-});
-
 // definição de status
+
+type player = {
+    name: string;
+    health: number;
+    mana: number;
+    damage: number;
+}
 
 type ally = {
     name: string;
     health: number;
-    mana: number;
+    energy: number;
     damage: number;
 }
 
@@ -48,18 +42,33 @@ type enemy = {
     damage: number;
 }
 
-let allys: ally[] = [
+const players: player[] = [
     {
-        name: "Main Character",
-        health: 150,
+        name: "Player One",
+        health: 200,
         mana: 30,
         damage: 30,
     },
+]
+
+const allys: ally[] = [
     {
-        name: "tutorial",
-        health: 100,
-        mana: 20,
-        damage: 20,
+        name: "Ally Monster",
+        health: 90,
+        energy: 30,
+        damage: 30,
+    },
+    {
+        name: "Ally Monster",
+        health: 80,
+        energy: 30,
+        damage: 30,
+    },
+    {
+        name: "Ally Monster",
+        health: 70,
+        energy: 30,
+        damage: 30,
     },
 ]
 
@@ -71,64 +80,131 @@ const enemys: enemy[] = [
     },
 ]
 
-function setAllyHp(ally: ally, characterHP: HTMLElement) {
-    characterHP.textContent = ally.health.toString();
+function setPlayerHp(player: player, playerHp: HTMLElement) {
+    playerHp.textContent = player.health.toString();
 }
 
-setAllyHp(allys[0], characterHP[0]);
-setAllyHp(allys[1], characterHP[1]);
+setPlayerHp(players[0], playerHp[0]);
 
-function setAllyMp(ally: ally, characterMP: HTMLElement) {
-    characterMP.textContent = ally.mana.toString();
+function setPlayerMp(player: player, playerMp: HTMLElement) {
+    playerMp.textContent = player.mana.toString();
 }
 
-setAllyMp(allys[0], characterMP[0]);
-setAllyMp(allys[1], characterMP[1]);
+setPlayerMp(players[0], playerMp[0]);
+
+function setAllyHp(ally: ally, allyHp: HTMLElement) {
+    allyHp.textContent = ally.health.toString();
+}
+
+setEnemyHp(allys[0], allyHp[0]);
+setEnemyHp(allys[1], allyHp[1]);
+setEnemyHp(allys[2], allyHp[2]);
 
 function setEnemyHp(enemy: enemy, EnemyHp: HTMLElement) {
     EnemyHp.textContent = enemy.health.toString();
 }
 
-setEnemyHp(enemys[0], EnemyHp[0]);
-setEnemyHp(enemys[0], EnemyHp[1]);
-setEnemyHp(enemys[0], EnemyHp[2]);
+setEnemyHp(enemys[0], enemyHp[0]);
+setEnemyHp(enemys[0], enemyHp[1]);
+setEnemyHp(enemys[0], enemyHp[2]);
 
 // sistema de vida
+
+function updatePlayerHPBar() {
+    const maxHP = 200; 
+    const healthPercentage = (players[0].health / maxHP) * 100;
+    playerHealthBar[0].style.width = `${healthPercentage}%`;
+}
+
+function updateAllyHPBar() {
+    const firstAllyMaxHP = 90;
+    const secondAllyMaxHp = 80;
+    const thirdAllyMaxHp = 70;
+
+    const healthPercentageOne = (allys[0].health / firstAllyMaxHP) * 100;
+    allyHpBar[0].style.width = `${healthPercentageOne}%`;
+
+    const healthPercentageTwo = (allys[1].health / secondAllyMaxHp) * 100;
+    allyHpBar[1].style.width = `${healthPercentageTwo}%`;
+
+    const healthPercentageThree = (allys[2].health / thirdAllyMaxHp) * 100;
+    allyHpBar[2].style.width = `${healthPercentageThree}%`;
+}
 
 function updateEnemyHPBar() {
     const maxHP = 100
     const healthPercentageOne = (enemys[0].health / maxHP) * 100;
-    hpBar[0].style.width = `${healthPercentageOne}%`;
+    enemyHpBar[0].style.width = `${healthPercentageOne}%`;
 
     const healthPercentageTwo = (enemys[0].health / maxHP) * 100;
-    hpBar[1].style.width = `${healthPercentageTwo}%`;
+    enemyHpBar[1].style.width = `${healthPercentageTwo}%`;
 
     const healthPercentageThree = (enemys[0].health / maxHP) * 100;
-    hpBar[2].style.width = `${healthPercentageThree}%`;
+    enemyHpBar[2].style.width = `${healthPercentageThree}%`;
 }
 
-function updateAllyHPBar() {
-    const maxHP = 150; 
-    const healthPercentage = (allys[0].health / maxHP) * 100;
-    healthBar[0].style.width = `${healthPercentage}%`;
-}
+
 
 // sistema de dano
+
+function reducePlayerHP() {
+    players[0].health -= 10
+
+    setPlayerHp(players[0], playerHp[0]);
+    updatePlayerHPBar();
+}
 
 function reduceEnemyHP() {
     enemys[0].health -= 10
 
-    setEnemyHp(enemys[0], EnemyHp[0]);
-    setEnemyHp(enemys[0], EnemyHp[1]);
-    setEnemyHp(enemys[0], EnemyHp[2]);
+    setEnemyHp(enemys[0], enemyHp[0]);
+    setEnemyHp(enemys[0], enemyHp[1]);
+    setEnemyHp(enemys[0], enemyHp[2]);
     updateEnemyHPBar();
 }
 
-function reduceAllyHP() {
+function reduceFirstAllyHP() {
     allys[0].health -= 10
 
-    setAllyHp(allys[0], characterHP[0]);
+    setAllyHp(allys[0], allyHp[0]);
     updateAllyHPBar();
+}
+
+function reduceSecondAllyHP() {
+    allys[1].health -= 10
+
+    setAllyHp(allys[1], allyHp[1]);
+    updateAllyHPBar();
+}
+
+function reduceThirdAllyHP() {
+    allys[2].health -= 10
+
+    setAllyHp(allys[2], allyHp[2]);
+    updateAllyHPBar();
+}
+
+// sistema de morte 
+
+function verifyFirstAllyHP() {
+    if (allyHp.item(0).textContent <= "0") {
+        ally.item(0).style.display = "none"
+        allyHp.item(0).style.display = "none"
+    }
+}
+
+function verifySecondAllyHP() {
+    if (allyHp.item(1).textContent <= "0") {
+        ally.item(1).style.display = "none"
+        allyHp.item(1).style.display = "none"
+    }
+}
+
+function verifyThirdAllyHP() {
+    if (allyHp.item(2).textContent <= "0") {
+        ally.item(2).style.display = "none"
+        allyHp.item(2).style.display = "none"
+    }  
 }
 
 // sistema de ataque aleatorio dos inimigos 
@@ -137,96 +213,180 @@ const firstEnemy = enemy.item(0);
 const secondEnemy = enemy.item(1);
 const thirdEnemy = enemy.item(2);
 
-function attacktutorial() {
-    setTimeout(() => {
-        firstEnemy.style.bottom = "25%"
-        firstEnemy.style.right = "70rem";
-        reduceAllyHP()
+function firstEnemyMonsterAttack() {
+    let randomAttackTarget = Math.floor(Math.random() * allys.length);
+    console.log (randomAttackTarget)
 
+    if (randomAttackTarget === 0 ) {
         setTimeout(() => {
-            firstEnemy.style.bottom = "0";
-            firstEnemy.style.right = "32rem";
-        }, 2000);
-
-    }, 500);
-
-   setTimeout(() => {
-        secondEnemy.style.bottom = "25%"
-        secondEnemy.style.right = "70rem";
-        reduceAllyHP()
-
-        setTimeout(() => {
-            secondEnemy.style.top = "0";
-            secondEnemy.style.right = "16rem";
-        }, 2000);
-
-   }, 1000);
-
-  setTimeout(()=>{
-    thirdEnemy.style.bottom = "25%"
-    thirdEnemy.style.right = "70rem";
-    reduceAllyHP()
-
-    setTimeout(() => {
-        thirdEnemy.style.bottom = "0";
-        thirdEnemy.style.right = "0";
-    }, 2000);
+            firstEnemy.style.bottom = "0"
+            firstEnemy.style.right = "70rem";
+            
     
-  }, 1500);
+            setTimeout(() => {
+                reduceFirstAllyHP()
+                verifyFirstAllyHP()
+                firstEnemy.style.bottom = "0";
+                firstEnemy.style.right = "32rem";
+            }, 2000);
+    
+        }, 500);
+    }
+
+    if (randomAttackTarget === 1 ) {
+        setTimeout(() => {
+            firstEnemy.style.bottom = "40%"
+            firstEnemy.style.right = "85rem";
+            
+    
+            setTimeout(() => {
+                reduceSecondAllyHP()
+                verifySecondAllyHP()
+                firstEnemy.style.bottom = "0";
+                firstEnemy.style.right = "32rem";
+            }, 2000);
+    
+        }, 500);
+    }
+
+    if (randomAttackTarget === 2 ) {
+        setTimeout(() => {
+            firstEnemy.style.bottom = "0"
+            firstEnemy.style.right = "100rem";
+    
+            setTimeout(() => {
+                reduceThirdAllyHP()
+                verifyThirdAllyHP()
+                firstEnemy.style.bottom = "0";
+                firstEnemy.style.right = "32rem";
+            }, 2000);
+    
+        }, 500);
+    
+    }
+
 }
 
-function attackMainCharacter() {
-    setTimeout(() => {
-        firstEnemy.style.bottom = "25%"
-        firstEnemy.style.right = "85rem";
-        reduceAllyHP()
+function secondEnemyMonsterAttack() {
+    let randomAttackTarget = Math.floor(Math.random() * allys.length);
+    console.log (randomAttackTarget)
 
+    if (randomAttackTarget === 0 ) {
         setTimeout(() => {
-            firstEnemy.style.bottom = "0";
-            firstEnemy.style.right = "32rem";
-        }, 2000);
-
-    }, 500);
-
-   setTimeout(() => {
-        secondEnemy.style.bottom = "25%"
-        secondEnemy.style.right = "85rem";
-        reduceAllyHP()
-
-        setTimeout(() => {
-            secondEnemy.style.top = "0";
-            secondEnemy.style.right = "16rem";
-        }, 2000);
-
-   }, 1000);
-
-  setTimeout(()=>{
-    thirdEnemy.style.bottom = "25%"
-    thirdEnemy.style.right = "85rem";
-    reduceAllyHP()
-
-    setTimeout(() => {
-        thirdEnemy.style.bottom = "0";
-        thirdEnemy.style.right = "0";
-    }, 2000);
+            secondEnemy.style.bottom = "0"
+            secondEnemy.style.right = "70rem";
+            
     
-  }, 1500);
+            setTimeout(() => {
+                reduceFirstAllyHP()
+                verifyFirstAllyHP()
+                secondEnemy.style.bottom = "40%"
+                secondEnemy.style.right = "16rem";
+            }, 2000);
+    
+       }, 4000);
+    }
+
+    if (randomAttackTarget === 1 ) {
+        setTimeout(() => {
+            secondEnemy.style.bottom = "40%"
+            secondEnemy.style.right = "85rem";
+            
+            setTimeout(() => {
+                reduceSecondAllyHP()
+                verifySecondAllyHP()
+                secondEnemy.style.bottom = "40%";
+                secondEnemy.style.right = "16rem";
+            }, 2000);
+    
+       }, 4000);
+    }
+    
+    if (randomAttackTarget === 2 ) {
+        setTimeout(() => {
+            secondEnemy.style.bottom = "0"
+            secondEnemy.style.right = "100rem";
+   
+        
+            setTimeout(() => {
+                reduceThirdAllyHP()
+                verifyThirdAllyHP()
+    
+                secondEnemy.style.bottom = "40%"
+                secondEnemy.style.right = "16rem";
+            }, 2000);
+        
+        }, 4000);
+    }
+}
+
+function thirdEnemyMonsterAttack() {
+    let randomAttackTarget = Math.floor(Math.random() * allys.length);
+    console.log (randomAttackTarget)
+
+    if (randomAttackTarget === 0 ) {
+        setTimeout(()=>{
+            thirdEnemy.style.bottom = "0"
+            thirdEnemy.style.right = "70rem";
+            
+        
+            setTimeout(() => {
+                reduceFirstAllyHP()
+                verifyFirstAllyHP()
+                thirdEnemy.style.bottom = "0";
+                thirdEnemy.style.right = "0";
+            }, 2000);
+            
+          }, 8000);
+    }
+
+    
+    if (randomAttackTarget === 1 ) {
+        setTimeout(()=>{
+            thirdEnemy.style.bottom = "40%"
+            thirdEnemy.style.right = "85rem";
+        
+            setTimeout(() => {
+                reduceSecondAllyHP()
+                verifySecondAllyHP()
+                thirdEnemy.style.bottom = "0";
+                thirdEnemy.style.right = "0";
+            }, 2000);
+            
+          }, 8000);
+    }
+
+    
+    if (randomAttackTarget === 2 ) {
+        setTimeout(()=>{
+            thirdEnemy.style.bottom = "0"
+            thirdEnemy.style.right = "100rem";
+           
+            setTimeout(() => {
+                reduceThirdAllyHP()
+                verifyThirdAllyHP()
+                thirdEnemy.style.bottom = "0";
+                thirdEnemy.style.right = "0";
+            }, 2000);
+            
+        }, 8000);
+    }   
 }
 
 // sistema de mana
 
-function updateManaBar(index: number) {
+function updatePlayerManaBar(index: number) {
     const maxMana = 30; 
-    const manaPercentage = (allys[index].mana / maxMana) * 100;
+    const manaPercentage = (players[index].mana / maxMana) * 100;
     manaBar[index].style.width = `${manaPercentage}%`;
 }
 
 function reduceMana(index: number) {
-    if (allys[index].mana >= 10) {
-        allys[index].mana -= 10;
+    if (players[index].mana >= 10) {
+        players[index].mana -= 10;
 
-        setAllyMp(allys[index], characterMP[index]);
-        updateManaBar(index);
+        setPlayerMp(players[index], playerMp[index]);
+        updatePlayerManaBar(index);
 
         
     } else {
@@ -235,11 +395,11 @@ function reduceMana(index: number) {
 }
 
 function increaseMana(index: number) {
-    if (allys[index].mana <= 20) {
-        allys[index].mana = allys[index].mana + 10;
+    if (players[index].mana <= 20) {
+        players[index].mana = players[index].mana + 10;
     }
-        setAllyMp(allys[index], characterMP[index]);
-        updateManaBar(index);
+        setPlayerMp(players[index], playerMp[index]);
+        updatePlayerManaBar(index);
 }
 
 const victoryScreen = document.getElementById('won') as HTMLElement;
@@ -252,7 +412,7 @@ const nextTurnButton = document.getElementById('end-turn-button') as HTMLButtonE
 if (nextTurnButton) {
     nextTurnButton.addEventListener('click', () => {
 
-        EnemyHp.forEach((e) => {
+        enemyHp.forEach((e) => {
         
             if (e.textContent === "0") {
                 enemy.forEach((e) =>{
@@ -260,25 +420,22 @@ if (nextTurnButton) {
                     victoryScreen.style.display = "block"
                 });
         
-                emptyHpBar.forEach((e)=>{
+                enemyEmptyHpBar.forEach((e)=>{
                     e.style.display = "none"
                 });
             }
 
         });
 
-        let randomAttackTarget = Math.floor(Math.random() * 2);
-        
-        if (randomAttackTarget === 0 ) {
-             attackMainCharacter() 
-        } else {
-            attacktutorial() 
-        }
-        
-        reduceEnemyHP()
+        firstEnemyMonsterAttack() 
+        secondEnemyMonsterAttack() 
+        thirdEnemyMonsterAttack()
+       
+        // reduceEnemyHP()
         increaseMana(0)
 
-        characterHP.forEach((e)=>{
+
+        playerHp.forEach((e)=>{
             if (e.textContent === "0") {
                 loseScreen.style.display = "block"
             } 
@@ -287,8 +444,6 @@ if (nextTurnButton) {
 
     });
 }
-
-
 
 // sistema de criar cartas na mão
 
